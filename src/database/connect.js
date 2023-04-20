3a0460a039facf77b74763ddc5190b19d0d43cab
MarioADS <marioalejandrodomi@gmail.com>
2023-04-20 13:11:10 -0600

    first workflow for actions

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo/operations_logs')
+mongoose.connect('mongodb://localhost:27017/operations_logs')
   .then( database => console.log('Database is conneted!') )
   .catch(error => console.error(`Database not connected: ${error.message}`) );
