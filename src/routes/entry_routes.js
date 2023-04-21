4b1fc99389095a144f0f67e3446e7082fc2fc307
MarioADS <marioalejandrodomi@gmail.com>
2023-04-21 07:47:01 -0600

    remove get operations service

--- src/routes/entry_routes.js
+++ src/routes/entry_routes.js
@@ -2,7 +2,7 @@ const exporess = require('express');
 const routes = exporess.Router();
 
 routes.get('/healtcheck', (req, res) => {
-  return res.json({ message: 'hello from digital ocean server', status: "ok" });
+  return res.json({ message: 'hello world', status: "ok" });
 });
 
 module.exports = routes;
