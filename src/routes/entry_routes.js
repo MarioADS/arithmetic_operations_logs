6ea73129914ef630d6fae5aee443b239284a4e2c
MarioADS <marioalejandrodomi@gmail.com>
2023-05-10 13:22:47 -0600

    example of deploy

--- src/routes/entry_routes.js
+++ src/routes/entry_routes.js
@@ -2,7 +2,7 @@ const exporess = require('express');
 const routes = exporess.Router();
 
 routes.get('/healtcheck', (req, res) => {
-  return res.json({ message: 'hello world', status: "ok" });
+  return res.json({ message: 'hello world from github actions', status: "ok" });
 });
 
 module.exports = routes;
