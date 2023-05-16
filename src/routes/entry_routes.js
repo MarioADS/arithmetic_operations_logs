aad2712eca2b0446f687b339247088c8924ab888
MarioADS <marioalejandrodomi@gmail.com>
2023-05-16 10:23:02 -0600

    message test github actions

--- src/routes/entry_routes.js
+++ src/routes/entry_routes.js
@@ -2,7 +2,7 @@ const exporess = require('express');
 const routes = exporess.Router();
 
 routes.get('/healtcheck', (req, res) => {
-  return res.json({ message: 'hello liverpool from github actions', status: "ok" });
+  return res.json({ message: 'hello friends from github actions, test!', status: "ok", code: 200 });
 });
 
 module.exports = routes;
